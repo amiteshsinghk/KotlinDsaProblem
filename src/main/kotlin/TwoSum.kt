7901e5c3ab8e4729392e2800742836c7a96be72e
amiteshsinghk <amiteshsinghk@gmail.com>
2024-06-11 19:02:43 +0530

    Two Sum

---
+++ src/main/kotlin/TwoSum.kt
@@ -0,0 +1,49 @@
+/**
+*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3:
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+
+* */
+
+fun main() {
+    val i= intArrayOf(3,2,4)
+    println(twoSum(i,6).joinToString())
+
+}
+
+fun twoSum(nums: IntArray, target: Int) : IntArray{
+    var arra = intArrayOf()
+    loop@  for(i in nums.indices){
+        for(j in i+1 until(nums.size)){
+            if(target == nums[i] + nums[j]){
+                var m = intArrayOf()
+                arra = arra.plus(i).plus(j)
+                break@loop
+            }
+        }
+
+    }
+    return arra
+}
