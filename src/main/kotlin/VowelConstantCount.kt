17afe9b0d338582b7e436f086cf6cf861587afd4
amiteshsinghk <amiteshsinghk@gmail.com>
2024-08-17 00:06:26 +0530

    Java Program to count the total number of vowels and consonants in a string.

--- src/main/kotlin/VowelConstantCount.kt
+++ src/main/kotlin/VowelConstantCount.kt
@@ -1,9 +1,17 @@
 //Java Program to count the total number of vowels and consonants in a string
 //This is a really simple sentence
 fun main(){
-    var item = countVowelConstant("This is a really simple sentence")
+    var item = countVowelsAndConsonantsUsingRegex("This is a really simple sentence")
     println("Vowels ${item.first}, Constant ${item.second}")
 }
+fun countVowelsAndConsonantsUsingRegex(s: String): Pair<Int, Int> {
+   val vowelRegex ="[aeiouAEIOU]".toRegex()
+    val constantRegex ="[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]".toRegex()
+    var vowelCount = vowelRegex.findAll(s).count()
+    var constantCount = constantRegex.findAll(s).count()
+    return Pair(vowelCount,constantCount)
+}
+
 fun countVowelConstant(s: String): Pair<Int, Int> {
     var vowels =0
     var constan =0
