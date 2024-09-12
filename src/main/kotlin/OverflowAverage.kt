0ea313eacfdddfef6cb8e09e09ae805b8c32d919
Amitesh Singh <amiteshsinghk@gmail.com>
2024-09-13 01:08:19 +0530

    Compute average of two numbers without overflow

--- src/main/kotlin/OverflowAverage.kt
+++ src/main/kotlin/OverflowAverage.kt
@@ -1,7 +1,6 @@
 // Compute average of two numbers without overflow
 // formula to safely compute the average without overflow:
 // average = (a/2) + (b/2) +((a % 2 +b % 2)/2)
-//
 
 fun main(){
     println(averageOfTwoNumbers(2147483646,2147483647))
