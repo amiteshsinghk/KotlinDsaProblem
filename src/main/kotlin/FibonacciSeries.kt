eed4dcb182d8202ae877f2ec1108e06c8687a753
Amitesh Singh <amiteshsinghk@gmail.com>
2024-09-12 17:48:33 +0530

    Write a kotlin Program to generate Fibonacci Triangle.

--- src/main/kotlin/FibonacciSeries.kt
+++ src/main/kotlin/FibonacciSeries.kt
@@ -2,8 +2,33 @@
 // In fibonacci series, next number is the sum of previous two numbers
 // for example 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 etc. The first two numbers of fibonacci series are 0 and 1.
 
+//Write a kotlin Program to generate Fibonacci Triangle.
+
 fun main(){
     printFibonacciSeries(10)
+    fibonacciTriangle(3)
+}
+
+//Write a kotlin Program to generate Fibonacci Triangle.
+//n= number of rows
+fun fibonacciTriangle(num: Int){
+    var firstNum = 1
+    var secondNum = 1
+    var sum: Int
+    println(firstNum)
+    print("$secondNum ")
+    for (i in 1..num){
+        var j = i
+        while (j>=0){
+            sum = firstNum + secondNum
+            print("$sum ")
+            firstNum = secondNum
+            secondNum = sum
+            j--
+        }
+        println()
+    }
+
 }
 
 fun printFibonacciSeries(num: Int){
