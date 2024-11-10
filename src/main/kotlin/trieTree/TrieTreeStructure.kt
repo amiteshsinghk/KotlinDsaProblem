9f7d84126137c1050d6cdbedc723745b13bcd699
Amitesh Singh <amiteshsinghk@gmail.com>
2024-11-10 20:08:33 +0530

    Update

--- src/main/kotlin/trieTree/TrieTreeStructure.kt
+++ src/main/kotlin/trieTree/TrieTreeStructure.kt
@@ -2,7 +2,7 @@ package trieTree
 
 class TrieTreeStructure {
 
-    class Node{
+    class Node {
         var children: Array<Node?> = arrayOfNulls(26)
         var eow: Boolean = false
 
@@ -10,40 +10,39 @@ class TrieTreeStructure {
 }
 
 var root = TrieTreeStructure.Node()
-fun insertTrieTree(word: String){
+fun insertTrieTree(word: String) {
     var current = root
-   for (i in word.indices){
-       // this will give the index of current letter
-       val index = word[i]-'a'
-       // If the current index of node is null than add a new node
-       if (current.children.get(index) == null){
-           //add new Node
-           current.children[index] = TrieTreeStructure.Node()
-       }
+    for (i in word.indices) {
+        // this will give the index of current letter
+        val index = word[i] - 'a'
+        // If the current index of node is null than add a new node
+        if (current.children[index] == null) {
+            //add new Node
+            current.children[index] = TrieTreeStructure.Node()
+        }
         // we need to make the eow of last node of word true, So that we know, word is ended here.
-       if (i == word.length-1){
-           current.children[index]?.eow = true
-       }
-       current = current.children[index]!!
-   }
+        if (i == word.length - 1) {
+            current.children[index]?.eow = true
+        }
+        current = current.children[index]!!
+    }
 }
 
-fun searchTrieTree(word: String): Boolean{
+fun searchTrieTree(word: String): Boolean {
     var current = root
-    for (i in word.indices){
+    for (i in word.indices) {
         val index = word[i] - 'a'
         if (current.children[index] == null) return false
-        if ((i == word.length-1) && current.children[index]?.eow == false) {
+        if ((i == word.length - 1) && current.children[index]?.eow == false) {
             return false
-
         }
         current = current.children[index]!!
     }
     return true
 }
 
-fun main(){
-    val word = arrayOf("the", "their","a", "there","any")
+fun main() {
+    val word = arrayOf("the", "their", "a", "there", "any")
     word.forEachIndexed { _, s ->
         insertTrieTree(s)
     }
