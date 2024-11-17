d5ecf603d2c76c3c76ae9b72db37b302d51e1841
Amitesh Singh <amiteshsinghk@gmail.com>
2024-11-17 15:27:53 +0530

    mergeTwoLists

---
+++ src/main/kotlin/trieTree/CountUniqueSubstrings.kt
@@ -0,0 +1,8 @@
+package trieTree
+
+/*
+Given a string of length n of lowercase alphabet chracters, we need to count total
+number of distinct substrings of this string.
+str = "ababa"
+ans = 10
+*/
