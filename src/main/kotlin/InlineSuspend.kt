281f612e8e1987e266f2ea9222a679bc82e4ec52
Amitesh Singh <amiteshsinghk@gmail.com>
2024-12-11 18:58:32 +0530

    inline suspend

---
+++ src/main/kotlin/InlineSuspend.kt
@@ -0,0 +1,20 @@
+/*
+* As per document suspending function call be from from another suspending function or form a coroutine scope.
+* A work around is this where we are passing a suspending function in a non suspending function using inline keyword.
+*
+* Here, passMeSuspendingFunction can take suspend as well as non suspend function and can do some action.
+* */
+suspend fun main(){
+    println(passMeSuspendingFunction{ printMe() } )
+}
+
+inline fun passMeSuspendingFunction(abc:()-> String): String{
+    return abc.invoke()
+}
+
+suspend fun printMe(): String{
+//    delay(1000)
+    return "Application :: I m suspending function "
+}
+
+
