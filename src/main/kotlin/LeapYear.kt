91cd057ed7bef4c56431ba114e546881b4629cef
Amitesh Singh <amiteshsinghk@gmail.com>
2024-12-30 21:48:34 +0530

    update

--- src/main/kotlin/LeapYear.kt
+++ src/main/kotlin/LeapYear.kt
@@ -2,9 +2,10 @@
 // The year is a leap year if “any one of ” the following conditions are satisfied:
 // 1. The year is multiple of 400.
 // 2. The year is a multiple of 4 and not a multiple of 100.
+// Leap years: 2000, 2004, 2008, 2012, 2016, 2020, 2024
 
 fun main(){
-    println(checkLeapYear(2100))
+    println(checkLeapYear(2024))
 }
 
 fun checkLeapYear(year: Int): Boolean{
