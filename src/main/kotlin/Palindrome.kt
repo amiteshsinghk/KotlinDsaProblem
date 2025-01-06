9fc6df2803502ecd20fbc3b46e722dbc8eeca220
Amitesh Singh <amiteshsinghk@gmail.com>
2025-01-06 14:41:57 +0530

    Palindrome

--- src/main/kotlin/Palindrome.kt
+++ src/main/kotlin/Palindrome.kt
@@ -1,11 +1,12 @@
 // Program to Check if a Given String is Palindrome
 
 fun main(){
+    println(isPalindrome("0P"))
     println(isPalindrome("A man, a plan, a canal: Panama"))
 }
 
 fun isPalindrome(s: String): Boolean{
-    val item = s.filter{it.isLetter()}.lowercase().toCharArray()
+    val item = s.filter{it.isLetter() || it.isDigit()}.lowercase().toCharArray()
     val itemLength = item.size
     var isPalim = true
     for (i in 0 until (itemLength)/2){
