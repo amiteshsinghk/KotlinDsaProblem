181d4d2b77d98be2c30a09e39f8a053e881eaba2
Amitesh Singh <amiteshsinghk@gmail.com>
2025-01-08 21:54:08 +0530

    MaxHeightContainer

--- src/main/kotlin/interviewQuestion/MaxHeightContainer.kt
+++ src/main/kotlin/interviewQuestion/MaxHeightContainer.kt
@@ -1,4 +1,4 @@
-package practo
+package interviewQuestion
 
 /*
 * You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
@@ -18,6 +18,7 @@ Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,
 fun main(){
     var list = listOf(1,8,6,2,5,4,8,3,7)
     println( maxArea(list))
+    
 }
 
 fun maxArea(height: List<Int>): Int{
